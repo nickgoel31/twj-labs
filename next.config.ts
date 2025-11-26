5d6ea52cd0c5d9d409b88edaf8d59248047a4b36
unknown <harshgoel2004.work@gmail.com>
2025-11-27 01:41:32 +0530

    bug fixes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  i18n:{
-    locales: ['en-US', 'fr-FR', 'de-DE', 'en-IN'],
-    defaultLocale: 'en-US',
-  },
   images:{
     domains: ['drive.google.com', 'cdn.sanity.io' ],
   }
